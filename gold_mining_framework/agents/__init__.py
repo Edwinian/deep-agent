c79af469ed3e42e72b587d7c64a2bf06253b307f
Edwin Chan <eneon54321@gmail.com>
2026-09-22 00:54:48 +0800

    added gold mining framework

---
+++ gold_mining_framework/agents/__init__.py
@@ -0,0 +1,5 @@
+"""Specialist agents used as nodes in the gold mining graph."""
+
+from gold_mining_framework.agents.idea_generation_agent import idea_generation_agent
+
+__all__ = ["idea_generation_agent"]
