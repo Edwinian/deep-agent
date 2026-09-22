5d1036198fb3388d20267f71131ece59e1a3a702
Edwin <eneon54321@gmail.com>
2026-09-22 11:04:01 +0800

    moved GMF to separate repo

--- gold_mining_framework/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Specialist agents used as nodes in the gold mining graph."""
-
-from gold_mining_framework.agents.idea_generation_agent import idea_generation_agent
-
-__all__ = ["idea_generation_agent"]
